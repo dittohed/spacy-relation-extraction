f2af34320031f93edc183347dcfbe9b1e634aba1
dittohed <wojtek.konieczkowicz@gmail.com>
2021-08-23 21:32:13 +0200

    Cleaning

--- tmp/test.py
+++ tmp/test.py
@@ -1,14 +1,13 @@
-# for quick tests (contains some examples also)
+# displays dependency tree for a given sentence
 import spacy
 from spacy import displacy
 
 # TODO: ładować plik linijka po linijce i tylko zmieniać indeks
 
---- display dependency tree example ---
 nlp = spacy.load('en_core_web_trf')
-doc = nlp('we hypothesised that eating home cooked meals more frequently would be associated with markers of a healthier diet and improved cardio-metabolic health')
+doc = nlp('study investigating interactions between genetic and lifestyle factors')
 # print(doc.ents)
-for word in doc:
-    print(word.text, word.lemma_)
+# for word in doc:
+#     print(word.text, word.lemma_)
 displacy.serve(doc, style='dep')
 # print([token.dep_ for token in doc])
